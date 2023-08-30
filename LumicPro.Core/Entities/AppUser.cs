db61f9e87731cb00d138a73b9589ab3a39e3bb74
Lumic-code <wisdomlumic0368@gmail.com>
2023-08-30 01:43:42 +0100

    added migrations

---
+++ LumicPro.Core/Entities/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using LumicPro.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace LumicPro.Core.Entities
+{
+    public class AppUser
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Lenght must be 3 - 50 characters")]
+        public string FirstName { get; set; }
+       
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Lenght must be 3 - 50 characters")]
+        public string LastName { get; set; }
+       
+        [Required]
+        public AttendanceStatus AttendanceStatus { get; set; }
+
+    }
+}
---
+++ LumicPro.Infrastructure/Context/LumicProContext.cs
@@ -0,0 +1,13 @@
+﻿using LumicPro.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LumicPro.Infrastructure.Context
+{
+    public class LumicProContext : DbContext
+    {
+        public LumicProContext(DbContextOptions<LumicProContext> options) : base(options)
+        {
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
